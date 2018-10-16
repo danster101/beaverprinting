d2bb9806a06a250fd5ed9bde768cdda21077dafb
Danial Hussain <dhussain101@gmail.com>
2018-10-15 22:36:29 -0400

    style edits

--- signup.php
+++ signup.php
@@ -136,73 +136,73 @@
                 <input type="tel" class="form-control" id="address" name="address" pattern="^[a-zA-Z0-9- ]+$" title="Please only enter letters and numbers" required value="<?php echo $address; ?>"/>
               </div>
             </div>
-              <div class="form-row">
-            <div class="col-md-6 mb-3">
-              <label for="city">City</label>
-              <input type="text" class="form-control" id="city" name="city" required value="<?php echo $city; ?>"/>
+            <div class="form-row">
+              <div class="col-md-6 mb-3">
+                <label for="city">City</label>
+                <input type="text" class="form-control" id="city" name="city" required value="<?php echo $city; ?>"/>
+              </div>
+              <div class="col-md-3 mb-3">
+                <label>State</label>
+                <select class="custom-select" name="state" required>
+                  <option value="">Select</option>
+                  <option value="Alabama">AL</option>
+                  <option value="Alaska">AK</option>
+                  <option value="Arizona">AZ</option>
+                  <option value="Arkansas">AR</option>
+                  <option value="California">CA</option>
+                  <option value="Colorado">CO</option>
+                  <option value="Connecticut">CT</option>
+                  <option value="Delaware">DE</option>
+                  <option value="District of Columbia">DC</option>
+                  <option value="Florida">FL</option>
+                  <option value="Georgia">GA</option>
+                  <option value="Hawaii">HI</option>
+                  <option value="Idaho">ID</option>
+                  <option value="Illinois">IL</option>
+                  <option value="Indiana">IN</option>
+                  <option value="Iowa">IA</option>
+                  <option value="Kansas">KS</option>
+                  <option value="Kentucky">KY</option>
+                  <option value="Louisiana">LA</option>
+                  <option value="Maine">ME</option>
+                  <option value="Maryland">MD</option>
+                  <option value="Massachusetts">MA</option>
+                  <option value="Michigan">MI</option>
+                  <option value="Minnesota">MN</option>
+                  <option value="Mississippi">MS</option>
+                  <option value="Missouri">MO</option>
+                  <option value="Montana">MT</option>
+                  <option value="Nebraska">NE</option>
+                  <option value="Nevada">NV</option>
+                  <option value="New Hampshire">NH</option>
+                  <option value="New Jersey">NJ</option>
+                  <option value="New Mexico">NM</option>
+                  <option value="New York">NY</option>
+                  <option value="North Carolina">NC</option>
+                  <option value="North Dakota">ND</option>
+                  <option value="Ohio">OH</option>
+                  <option value="Oklahoma">OK</option>
+                  <option value="Oregon">OR</option>
+                  <option value="Pennsylvania">PA</option>
+                  <option value="Rhode Island">RI</option>
+                  <option value="South Carolina">SC</option>
+                  <option value="South Dakota">SD</option>
+                  <option value="Tennessee">TN</option>
+                  <option value="Texas">TX</option>
+                  <option value="Utah">UT</option>
+                  <option value="Vermont">VT</option>
+                  <option value="Virginia">VA</option>
+                  <option value="Washington">WA</option>
+                  <option value="West Virginia">WV</option>
+                  <option value="Wisconsin">WI</option>
+                  <option value="Wyoming">WY</option>
+                </select>
+              </div>
+              <div class="col-md-3 mb-3">
+                <label for="validationCustom05">Zip (5 digits)</label>
+                <input type="text" class="form-control" id="zip" name="zip" pattern="^\d{5}$" title="Please enter a 5-digit zip code" required value="<?php echo $zip; ?>">
+              </div>
             </div>
-            <div class="col-md-3 mb-3">
-              <label>State</label>
-              <select class="custom-select" name="state" required>
-                <option value="">Select</option>
-                <option value="Alabama">AL</option>
-                <option value="Alaska">AK</option>
-                <option value="Arizona">AZ</option>
-                <option value="Arkansas">AR</option>
-                <option value="California">CA</option>
-                <option value="Colorado">CO</option>
-                <option value="Connecticut">CT</option>
-                <option value="Delaware">DE</option>
-                <option value="District of Columbia">DC</option>
-                <option value="Florida">FL</option>
-                <option value="Georgia">GA</option>
-                <option value="Hawaii">HI</option>
-                <option value="Idaho">ID</option>
-                <option value="Illinois">IL</option>
-                <option value="Indiana">IN</option>
-                <option value="Iowa">IA</option>
-                <option value="Kansas">KS</option>
-                <option value="Kentucky">KY</option>
-                <option value="Louisiana">LA</option>
-                <option value="Maine">ME</option>
-                <option value="Maryland">MD</option>
-                <option value="Massachusetts">MA</option>
-                <option value="Michigan">MI</option>
-                <option value="Minnesota">MN</option>
-                <option value="Mississippi">MS</option>
-                <option value="Missouri">MO</option>
-                <option value="Montana">MT</option>
-                <option value="Nebraska">NE</option>
-                <option value="Nevada">NV</option>
-                <option value="New Hampshire">NH</option>
-                <option value="New Jersey">NJ</option>
-                <option value="New Mexico">NM</option>
-                <option value="New York">NY</option>
-                <option value="North Carolina">NC</option>
-                <option value="North Dakota">ND</option>
-                <option value="Ohio">OH</option>
-                <option value="Oklahoma">OK</option>
-                <option value="Oregon">OR</option>
-                <option value="Pennsylvania">PA</option>
-                <option value="Rhode Island">RI</option>
-                <option value="South Carolina">SC</option>
-                <option value="South Dakota">SD</option>
-                <option value="Tennessee">TN</option>
-                <option value="Texas">TX</option>
-                <option value="Utah">UT</option>
-                <option value="Vermont">VT</option>
-                <option value="Virginia">VA</option>
-                <option value="Washington">WA</option>
-                <option value="West Virginia">WV</option>
-                <option value="Wisconsin">WI</option>
-                <option value="Wyoming">WY</option>
-              </select>
-            </div>
-            <div class="col-md-3 mb-3">
-              <label for="validationCustom05">Zip (5 digits)</label>
-              <input type="text" class="form-control" id="zip" name="zip" pattern="^\d{5}$" title="Please enter a 5-digit zip code" required value="<?php echo $zip; ?>">
-            </div>
-  </div>
             <div id="success"></div>
             <!-- For success/fail messages -->
             <button type="submit" class="btn btn-primary" id="signUpButton">Sign Up</button>
